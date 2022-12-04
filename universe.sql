ea55c3a39ad1c96ff8f7876e08b8ac7213c3b88c
TulayB <86021683+tulay99@users.noreply.github.com>
2022-12-04 15:04:03 +0300

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -276,24 +276,108 @@ INSERT INTO public.evren VALUES ('dep', 3, 54, NULL, NULL);
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.galaxy VALUES (1, 1.2, 'aa', 'aa', 1);
+INSERT INTO public.galaxy VALUES (2, 1.3, 'ab', 'ab', 2);
+INSERT INTO public.galaxy VALUES (3, 1.4, 'ac', 'ac', 3);
+INSERT INTO public.galaxy VALUES (4, 1.5, 'ad', 'ad', 4);
+INSERT INTO public.galaxy VALUES (5, 1.5, 'ae', 'ae', 5);
+INSERT INTO public.galaxy VALUES (6, 1.6, 'af', 'af', 6);
+INSERT INTO public.galaxy VALUES (7, 1.7, 'ag', 'ag', 7);
+INSERT INTO public.galaxy VALUES (8, 1.8, 'ah', 'ah', 8);
+INSERT INTO public.galaxy VALUES (9, 1.9, 'ai', 'ai', 9);
+INSERT INTO public.galaxy VALUES (10, 1.1, 'aj', 'aj', 10);
+INSERT INTO public.galaxy VALUES (11, 1.1, 'ak', 'ak', 11);
+INSERT INTO public.galaxy VALUES (12, 1.1, 'al', 'al', 12);
+INSERT INTO public.galaxy VALUES (13, 1.1, 'am', 'am', 13);
+INSERT INTO public.galaxy VALUES (14, 1.1, 'an', 'an', 14);
+INSERT INTO public.galaxy VALUES (15, 1.2, 'ap', 'ap', 15);
+INSERT INTO public.galaxy VALUES (16, 1.2, 'ar', 'ar', 16);
+INSERT INTO public.galaxy VALUES (17, 1.2, 'as', 'as', 17);
+INSERT INTO public.galaxy VALUES (18, 1.2, 'at', 'at', 18);
+INSERT INTO public.galaxy VALUES (19, 1.2, 'au', 'au', 19);
+INSERT INTO public.galaxy VALUES (20, 1.2, 'av', 'av', 20);
+INSERT INTO public.galaxy VALUES (21, 1.2, 'aw', 'aw', 21);
+INSERT INTO public.galaxy VALUES (22, 1.2, 'az', 'az', 22);
 
 
 --
 -- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.moon VALUES (1, 1.1, 'ma', 'ma  ', 1);
+INSERT INTO public.moon VALUES (2, 1.2, 'mb', 'mb  ', 2);
+INSERT INTO public.moon VALUES (3, 1.3, 'mc', 'mc  ', 3);
+INSERT INTO public.moon VALUES (4, 1.1, 'mdp', 'md  ', 4);
+INSERT INTO public.moon VALUES (5, 1.5, 'me', 'me  ', 5);
+INSERT INTO public.moon VALUES (6, 1.6, 'mf', 'mf  ', 6);
+INSERT INTO public.moon VALUES (7, 1.7, 'mg', 'mg  ', 7);
+INSERT INTO public.moon VALUES (8, 1.8, 'mh', 'mh  ', 8);
+INSERT INTO public.moon VALUES (9, 1.9, 'mi', 'mi  ', 9);
+INSERT INTO public.moon VALUES (10, 1.1, 'mj', 'mj  ', 10);
+INSERT INTO public.moon VALUES (11, 1.1, 'mk', 'mk  ', 11);
+INSERT INTO public.moon VALUES (12, 1.1, 'ml', 'ml  ', 12);
+INSERT INTO public.moon VALUES (13, 1.1, 'mm', 'mm  ', 13);
+INSERT INTO public.moon VALUES (14, 1.1, 'mo', 'mo  ', 14);
+INSERT INTO public.moon VALUES (15, 1.2, 'msh', 'msh ', 15);
+INSERT INTO public.moon VALUES (16, 1.2, 'msi', 'smi ', 16);
+INSERT INTO public.moon VALUES (17, 1.2, 'msj', 'mjs ', 17);
+INSERT INTO public.moon VALUES (18, 1.2, 'msk', 'msk ', 18);
+INSERT INTO public.moon VALUES (19, 1.2, 'msl', 'msl ', 19);
+INSERT INTO public.moon VALUES (20, 1.2, 'msm', 'msm ', 20);
 
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.planet VALUES (1, 1.1, 'pa', 1, 1);
+INSERT INTO public.planet VALUES (2, 1.2, 'pb', 2, 2);
+INSERT INTO public.planet VALUES (3, 1.3, 'pc', 3, 3);
+INSERT INTO public.planet VALUES (4, 1.4, 'pd', 4, 4);
+INSERT INTO public.planet VALUES (5, 1.5, 'pe', 5, 5);
+INSERT INTO public.planet VALUES (6, 1.6, 'pf', 6, 6);
+INSERT INTO public.planet VALUES (7, 1.7, 'pg', 7, 7);
+INSERT INTO public.planet VALUES (11, 1.1, 'ph', 11, 11);
+INSERT INTO public.planet VALUES (12, 1.1, 'pi', 12, 12);
+INSERT INTO public.planet VALUES (13, 1.1, 'pj', 13, 13);
+INSERT INTO public.planet VALUES (14, 1.1, 'pk', 14, 14);
+INSERT INTO public.planet VALUES (15, 1.2, 'pl', 15, 15);
+INSERT INTO public.planet VALUES (16, 1.2, 'pm', 16, 16);
+INSERT INTO public.planet VALUES (17, 1.2, 'pn', 17, 17);
+INSERT INTO public.planet VALUES (18, 1.2, 'po', 18, 18);
+INSERT INTO public.planet VALUES (19, 1.2, 'pr', 19, 19);
+INSERT INTO public.planet VALUES (20, 1.2, 'ps', 20, 20);
+INSERT INTO public.planet VALUES (8, 1.2, 'spk', 8, 8);
+INSERT INTO public.planet VALUES (9, 1.9, 'spl', 9, 9);
+INSERT INTO public.planet VALUES (10, 1.1, 'pms', 10, 10);
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.star VALUES (1, 1.1, 1, 'aa', 1);
+INSERT INTO public.star VALUES (2, 1.2, 2, 'ab', 2);
+INSERT INTO public.star VALUES (3, 1.3, 3, 'ac', 3);
+INSERT INTO public.star VALUES (4, 1.4, 4, 'ad', 4);
+INSERT INTO public.star VALUES (5, 1.5, 5, 'ae', 5);
+INSERT INTO public.star VALUES (6, 1.6, 6, 'af', 6);
+INSERT INTO public.star VALUES (7, 1.7, 7, 'ag', 7);
+INSERT INTO public.star VALUES (11, 1.1, 11, 'an', 11);
+INSERT INTO public.star VALUES (12, 1.1, 12, 'ah', 12);
+INSERT INTO public.star VALUES (13, 1.1, 13, 'ai', 13);
+INSERT INTO public.star VALUES (14, 1.1, 14, 'aj', 14);
+INSERT INTO public.star VALUES (15, 1.2, 15, 'ak', 15);
+INSERT INTO public.star VALUES (16, 1.2, 16, 'al', 16);
+INSERT INTO public.star VALUES (17, 1.2, 17, 'am', 17);
+INSERT INTO public.star VALUES (18, 1.2, 18, 'ao', 18);
+INSERT INTO public.star VALUES (19, 1.2, 19, 'ap', 19);
+INSERT INTO public.star VALUES (20, 1.2, 20, 'ar', 20);
+INSERT INTO public.star VALUES (21, 1.2, 21, 'as', 21);
+INSERT INTO public.star VALUES (22, 1.2, 22, 'at', 22);
+INSERT INTO public.star VALUES (8, 1.8, 8, 'sa', 8);
+INSERT INTO public.star VALUES (9, 1.9, 9, 'sp', 9);
+INSERT INTO public.star VALUES (10, 1.1, 10, 'sr', 10);
 
 
 --
@@ -438,4 +522,3 @@ ALTER TABLE ONLY public.star
 --
 -- PostgreSQL database dump complete
 --
-
